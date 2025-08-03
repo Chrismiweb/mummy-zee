d1e052e89821ba896513562c7a061a22a988b472
chrismiWeb <chrismibiteso@gmail.com>
2025-08-02 20:02:39 -0700

    added whatsap float

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import About from './About'
 import Navbar from '../../component/Navbar'
 import Footer from '../../component/Footer'
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
+
 
 function AboutPage() {
   return (
     <div>
+        <FloatingWhatsApp
+        phoneNumber="+2347035722334" // Replace with your WhatsApp number
+        accountName="Mummy Zee Store" // The display name
+        avatar="/image/mummyZee Logo.png" // Profile image URL
+        chatMessage="Hi, welcome to Mummy Zee Store, how can we help you today ? 🙂" // Initial message
+        statusMessage="Online"
+        placeholder="Type your message..."
+        darkMode={false}
+        allowClickAway
+        notification
+        notificationSound
+    />
         <Navbar/>
         <About/>
         <Footer />
